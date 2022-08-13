2e42c13989598c4c6123565d030ae29107e00119
Jose Lupianez <joselupianez@fullsail.edu>
2022-08-13 17:16:13 -0400

    Added Multiple Challenges
    Square(n) Sum, Sum of positive, Sum of two lowest positive integers, Odd or Even?, Multiples of 3 or 5, Create Phone Number

---
+++ 7kyu/oddOrEven.js
@@ -0,0 +1,13 @@
+// Odd or Even?
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Solution:
+function oddOrEven(array){ 
+    let sum = array.reduce((acc, cur) => acc + cur, 0);
+    return sum % 2 === 0 ? 'even' : 'odd';
+}
