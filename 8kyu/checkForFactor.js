7f3a0d562b418b836efa0bdf106b2b747cae61ba
Jose Lupianez <joselupianez@fullsail.edu>
2022-08-16 08:30:15 -0400

    Added Classy Classes and Check For Factor

---
+++ 8kyu/checkForFactor.js
@@ -0,0 +1,8 @@
+// Grasshopper - Check for factor
+
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// Solution:
+const checkForFactor = (base, factor) => base % factor == 0
