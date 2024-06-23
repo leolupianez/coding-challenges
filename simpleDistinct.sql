670f8d45e5ee8cf2c047caf987ae7540e4d397df
leolupianez <4380557+leolupianez@users.noreply.github.com>
2024-06-23 14:08:43 -0400

    Added new code challenges solved

---
+++ simpleDistinct.sql
@@ -0,0 +1,11 @@
+-- For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+-- people table schema
+
+--     id
+--     name
+--     age
+
+-- select table schema
+
+--     age (distinct)
+select distinct age from people
